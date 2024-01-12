503d4124d412c58adc2a81c4409be883d6cf80cc
jay <daonj0715@gmail.com>
2024-01-12 14:21:41 +0900

    init

---
+++ src/test/java/com/example/demae/DemaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
