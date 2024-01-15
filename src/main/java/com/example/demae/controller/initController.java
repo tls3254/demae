705a2986241a5ffd018f04ad976c8a8bfa64120f
jay <daonj0715@gmail.com>
2024-01-15 17:30:57 +0900

    main:daon

--- src/main/java/com/example/demae/controller/initController.java
+++ src/main/java/com/example/demae/controller/initController.java
@@ -1,9 +1,6 @@
 package com.example.demae.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/test/java/com/example/demae/DemaeApplicationTests.java
+++ src/test/java/com/example/demae/DemaeApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.demae;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootTest
