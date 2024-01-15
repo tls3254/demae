bc4a817fc76b39d7fc4fa5743f75b4adceadd4f1
jay <daonj0715@gmail.com>
2024-01-15 10:57:39 +0900

    fit:sss

--- src/test/java/com/example/demae/DemaeApplicationTests.java
+++ src/test/java/com/example/demae/DemaeApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demae;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootTest
 class DemaeApplicationTests {
 
     @Test
