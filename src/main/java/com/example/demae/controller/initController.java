b89bb6d93d6e6577e42da28e00cc53a971892803
jay <daonj0715@gmail.com>
2024-01-15 11:04:07 +0900

    fit:sss

---
+++ src/main/java/com/example/demae/controller/initController.java
@@ -0,0 +1,15 @@
+package com.example.demae.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class initController {
+    @GetMapping("/test")
+    public String test(){
+        return "test!!!";
+    }
+}
