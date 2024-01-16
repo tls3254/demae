888abf1980fb95a8639f07e3d9658cd620d71322
leegitjw <bachu7410@gmail.com>
2024-01-16 11:17:16 +0900

    에러 정리중

---
+++ src/main/java/com/example/demae/entity/Cart.java
@@ -0,0 +1,4 @@
+package com.example.demae.entity;
+
+public class Cart {
+}
---
+++ src/main/java/com/example/demae/repository/CartRepository.java
@@ -0,0 +1,6 @@
+package com.example.demae.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
