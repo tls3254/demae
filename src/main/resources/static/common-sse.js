34e4c2c795a5da0d8046236a00b72cf2bac1c3c0
leegitjw <bachu7410@gmail.com>
2024-01-18 03:49:54 +0900

    fix : sse 적용중

--- src/main/resources/static/common-sse.js
+++ src/main/resources/static/common-sse.js
@@ -3,6 +3,7 @@ const eventSource = new EventSource("/api/orders/connect");
 eventSource.onmessage = function (event) {
 	const notification = JSON.parse(event.data);
 	// 알림 처리 로직
+	alert(event.data)
 	console.log("Notification received:", notification);
 };
 
