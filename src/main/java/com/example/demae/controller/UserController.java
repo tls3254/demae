6ea59f9fa1af19395cd0a67ebc8de4dc7a52c25d
jay <daonj0715@gmail.com>
2024-01-17 10:59:07 +0900

    fix:merge from dev to main

--- src/main/java/com/example/demae/controller/UserController.java
+++ src/main/java/com/example/demae/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.example.demae.controller;
 
 import com.example.demae.dto.login.SignupRequestDto;
+import com.example.demae.entity.User;
+import com.example.demae.security.UserDetailsImpl;
 import com.example.demae.service.UserService;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,30 +15,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/users")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserController {
-
-    private final UserService userService;
-
-
+    private UserService userService;
     @GetMapping
     public String signUpAdrees(){
         return "signUp";
     }
 
     @PostMapping
-    public String signUpAdrees1(@ModelAttribute SignupRequestDto signupRequestDto, Model model){
-        System.out.println(signupRequestDto.getName());
+    public String signUpAdrees1(@ModelAttribute SignupRequestDto signupRequestDto){
         userService.signup(signupRequestDto);
         return "users";
     }
     @GetMapping("/loginForm")
-    public String AA(){
+    public String loginForm(){
         return "login";
     }
 
     @GetMapping("/main")
-    public String main() {
+    public String main(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
+        User user = userDetails.getUser();
+        if (user.getRole().name().equals("ADMIN") && user.getStore() != null) {
+            model.addAttribute("storeId", user.getStore().getId());
+            return "adminMain";
+        }
+        if (user.getRole().name().equals("ADMIN") && user.getStore() == null) {
+            return "adminMain";
+        }
         return "main";
     }
 }
