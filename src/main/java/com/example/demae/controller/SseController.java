a640af210bc07484a218f2006f7cb4cb551dac8d
leegitjw <bachu7410@gmail.com>
2024-01-18 20:09:15 +0900

    fix : sse 적용

--- src/main/java/com/example/demae/controller/SseController.java
+++ src/main/java/com/example/demae/controller/SseController.java
@@ -38,17 +38,6 @@ public class SseController {
 	private final StoreService storeService;
 	@GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public SseEmitter connect(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-		// 유저가 SSE 연결을 요청할 때 사용
-//		String id = String.valueOf(userDetails.getUser().getId());
-//		if(userEmitters.containsKey(String.valueOf(id))) {
-//			SseEmitter sseEmitter = userEmitters.get(id);
-//			userEmitters.remove(sseEmitter);
-//		}
-//
-//		SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
-//		userEmitters.put(id, emitter);
-//		emitter.onCompletion(() -> userEmitters.remove(id, emitter));
-//		emitter.onTimeout(() -> userEmitters.remove(id, emitter));
 		SseEmitter emitter = sseService.createConnect(userDetails.getUser().getId());
 		return emitter;
 	}
@@ -106,7 +95,6 @@ public class SseController {
 				}
 			}
 		}catch (IOException j){
-
 		}
 	}
 }
