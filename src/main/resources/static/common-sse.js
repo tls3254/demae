8af258040605bbc6ed3f8d142c1d93d0b54fc12c
leegitjw <bachu7410@gmail.com>
2024-01-18 16:25:18 +0900

    fix : sse 적용

--- src/main/resources/static/common-sse.js
+++ src/main/resources/static/common-sse.js
@@ -1,9 +1,11 @@
 const eventSource = new EventSource("/api/sse/connect");
 
 eventSource.onmessage = function (event) {
-	const notification = JSON.parse(event.data);
-	alert("알림 메시지")
-	console.log("Notification received:", notification);
+	const notification = JSON.stringify(event.data);
+	console.log(notification.message)
+	alert(notification)
+	window.location.href = '/api/orders';
+
 };
 
 console.log("EventSource created successfully.");
