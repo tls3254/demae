7df4725ed401e077a385defa804a9436c69b4e27
tls3254 <bikim9923@gmail.com>
2024-05-30 14:23:08 +0900

    refactor : ReviewRepository 수정

--- src/main/java/com/example/demae/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/demae/domain/review/repository/ReviewRepository.java
@@ -1,10 +1,11 @@
-package com.example.demae.repository;
+package com.example.demae.domain.review.repository;
 
-import com.example.demae.entity.Review;
+import com.example.demae.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review,Long> {
-    List<Review> findByOrderId(Long orderId);
+
+    List<Review> findByCart_CartId(Long cartId);
 }
