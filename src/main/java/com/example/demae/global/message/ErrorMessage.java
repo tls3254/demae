eb863402989a547e9a90edff94199684ed36eb79
tls3254 <bikim9923@gmail.com>
2024-05-30 14:12:26 +0900

    refactor : 새로운 메시지 추가

--- src/main/java/com/example/demae/global/message/ErrorMessage.java
+++ src/main/java/com/example/demae/global/message/ErrorMessage.java
@@ -1,7 +1,8 @@
 package com.example.demae.global.message;
 
 public enum ErrorMessage {
-    NOT_EXIST_TOKEN_ERROR_MESSAGE("토큰을 찾을 수 없습니다.");
+    NOT_EXIST_TOKEN_ERROR_MESSAGE("토큰을 찾을 수 없습니다."),
+    NOT_EXIST_EMAIL_ERROR_MESSAGE("존재하지 않는 이메일입니다.");
     private final String errorMessage;
 
     ErrorMessage(String errorMessage) {
